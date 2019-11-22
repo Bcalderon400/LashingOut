05ef6cf7c0023d4f68be3bc7bf4315aef14cc57a
Brian <briancalderon200@gmail.com>
2019-11-21 18:54:59 -0800

    react components

---
+++ SignIn.js
@@ -0,0 +1,12 @@
+class SignIn extends React.Component {
+  render() {
+    return (
+      <div>
+        <li className="nav-item">
+          <a href="#">Sign In</a>
+        </li>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<SignIn />, document.getElementById("SignIn"));
